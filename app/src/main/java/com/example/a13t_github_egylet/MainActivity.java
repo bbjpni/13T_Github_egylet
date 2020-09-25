a4132e4c4415239a47ab820450570a52876fd77e
Tomi <tamashahm99@gmail.com>
2020-09-25 14:02:48 +0200

    init...

--- app/src/main/java/com/example/a13t_github_egylet/MainActivity.java
+++ app/src/main/java/com/example/a13t_github_egylet/MainActivity.java
@@ -11,5 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void init(){}
+    public void init(){
+        //
+    }
 }
