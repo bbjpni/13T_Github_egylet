e9a461f2ecd115989fe45fb6cea1d0baab45af7b
Tomi <tamashahm99@gmail.com>
2020-09-25 13:58:55 +0200

    Initial commit

---
+++ app/src/main/java/com/example/a13t_github_egylet/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.a13t_github_egylet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
